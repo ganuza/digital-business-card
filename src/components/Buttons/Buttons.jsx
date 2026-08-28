32f61ed26abf0e53d68463f2b2e2429a592151d0
Adrian Zabolitzki <adrianzab@gmail.com>
2026-08-28 11:40:28 -0600

    Style Buttons section

--- src/components/Buttons/Buttons.jsx
+++ src/components/Buttons/Buttons.jsx
@@ -4,29 +4,32 @@ import { FaAddressCard, FaLinkedin, FaEnvelope } from 'react-icons/fa';
 function Buttons() {
   return (
     <section className="buttons section">
-      <a
-        href="/adrian-zabolitzki.vcf"
-        download="Adrian-Zabolitzki.vcf"
-        className="contact button"
-      >
-        <FaAddressCard />
-        Save Contact
-      </a>
+      <article>
+        <a
+          href="/adrian-zabolitzki.vcf"
+          download="Adrian-Zabolitzki.vcf"
+          className="contact button"
+        >
+          <FaAddressCard />
+          Save Contact
+        </a>
+      </article>
+      <article>
+        <a href="mailto:adrianzab@gmail.com" className="email button">
+          <FaEnvelope />
+          Email
+        </a>
 
-      <a href="mailto:adrianzab@gmail.com" className="email button">
-        <FaEnvelope />
-        Email
-      </a>
-
-      <a
-        href="https://www.linkedin.com/in/adrian-zabolitzki/"
-        target="_blank"
-        rel="noopener noreferrer"
-        className="linkedin button"
-      >
-        <FaLinkedin />
-        LinkedIn
-      </a>
+        <a
+          href="https://www.linkedin.com/in/adrian-zabolitzki/"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="linkedin button"
+        >
+          <FaLinkedin />
+          LinkedIn
+        </a>
+      </article>
     </section>
   );
 }
