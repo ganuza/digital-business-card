06839d7ca87c8b62fc2f0a3051488747983c1d04
Adrian Zabolitzki <adrianzab@gmail.com>
2026-08-28 10:51:20 -0600

    Add link to portfolio site

--- src/components/Info/Info.jsx
+++ src/components/Info/Info.jsx
@@ -5,7 +5,9 @@ function Info() {
     <section className="info section">
       <h1 className="info-header">Adrian Zabolitzki</h1>
       <h3 className="info-role">Software Developer</h3>
-      <p className="website-address">www.adrianz.dev</p>
+      <a href="www.adrianz.dev" className="website-address">
+        Portfolio Site
+      </a>
     </section>
   );
 }
