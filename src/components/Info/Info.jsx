896948a4821cd859d353fdff7e9d1a083b4485c2
Adrian Zabolitzki <adrianzab@gmail.com>
2026-08-28 11:16:02 -0600

    Fix portfolio site link

--- src/components/Info/Info.jsx
+++ src/components/Info/Info.jsx
@@ -5,7 +5,12 @@ function Info() {
     <section className="info section">
       <h1 className="info-header">Adrian Zabolitzki</h1>
       <h3 className="info-role">Software Developer</h3>
-      <a href="www.adrianz.dev" className="website-address">
+      <a
+        href="https://www.adrianz.dev"
+        target="_blank"
+        rel="noopener noreferrer"
+        className="website-address"
+      >
         Portfolio Site
       </a>
     </section>
